44eba7309581e7289e03bd23d493a50981a06c9d
soham <som.insomniacs@gmail.com>
2018-09-07 22:04:19 +0530

    Rotate screen-Clear view

--- app/src/main/java/com/example/soham/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/soham/activitylifecycle/MainActivity.java
@@ -1,17 +1,59 @@
 package com.example.soham.activitylifecycle;
 
+import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
+
+    private EditText editText;
+    private TextView textView;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        editText = (EditText) findViewById(R.id.txt_str);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Welcome " + editText.getText().toString());
+                button.setText("LOGOUT");
+            }
+        });
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        Log.d(TAG, "onRestoreInstanceState");
+
+//        if (savedInstanceState != null){
+//            editText.setText(savedInstanceState.getString("text"));
+//        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState, PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        Log.d(TAG, "onSaveInstanceState");
+
+//        outState.putString("text", editText.getText().toString());
     }
 
     @Override
@@ -43,4 +85,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStop();
         Log.d(TAG, "onStop");
     }
+
+
 }
